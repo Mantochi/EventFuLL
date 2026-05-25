277477c8d83816654ef073dc9dd1e361c18fe30a
Gabriel M <tochimike5@gmail.com>
2026-05-25 09:08:28 +0100

    fixing the root

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
+    <BrowserRouter basename='/EventFuLL'>
       <App />
     </BrowserRouter>
 )
